346b1e1cab2bdbaabeebadf5d085cba3808acf37
石文文 <Shiwenwen@shiwenwendembp.airdream>
2016-01-08 16:42:01 +0800

    Initial Commit

---
+++ MCChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCChat
+//
+//  Created by 石文文 on 16/1/8.
+//  Copyright © 2016年 shiwenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MCChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MCChat
+//
+//  Created by 石文文 on 16/1/8.
+//  Copyright © 2016年 shiwenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
