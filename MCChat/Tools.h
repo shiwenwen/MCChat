3209556a1bb34594729464fdd6491ae1c8c690d2
石文文 <shiwenwenDevelop@163.com>
2016-01-16 13:13:48 +0800

    语音功能完成

---
+++ MCChat/Tools.h
@@ -0,0 +1,13 @@
+//
+//  Tools.h
+//  MCChat
+//
+//  Created by 石文文 on 16/1/16.
+//  Copyright © 2016年 shiwenwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tools : NSObject
+
+@end
---
+++ MCChat/Tools.m
@@ -0,0 +1,13 @@
+//
+//  Tools.m
+//  MCChat
+//
+//  Created by 石文文 on 16/1/16.
+//  Copyright © 2016年 shiwenwen. All rights reserved.
+//
+
+#import "Tools.h"
+
+@implementation Tools
+
+@end
